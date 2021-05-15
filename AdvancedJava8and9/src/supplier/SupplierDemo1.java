bf16e042af32ebd700bf097b95441df08d2af459
shailendra <srmatkar@gmail.com>
2021-05-15 10:51:44 +0530

    again introducing change

--- AdvancedJava8and9/src/supplier/SupplierDemo1.java
+++ AdvancedJava8and9/src/supplier/SupplierDemo1.java
@@ -4,6 +4,8 @@ import java.util.Date;
 import java.util.function.Supplier;
 
 public class SupplierDemo1 {
+	
+	// Again introducing comment
 
 	public static void main(String[] args) {
 		Supplier<Date> s = ()->new Date();
